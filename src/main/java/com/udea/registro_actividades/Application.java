d9f0e4bce7bcfa155fcfb965360c2b7279295fad
Sebas Monsalve <sebas.monsalve@gmail.com>
2017-11-27 11:53:44 -0500

    Allow-Control-Allow-Origin

--- src/main/java/com/udea/registro_actividades/Application.java
+++ src/main/java/com/udea/registro_actividades/Application.java
@@ -1,11 +1,18 @@
 package com.udea.registro_actividades;
 
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication 
+@ComponentScan(basePackages="com.udea.registro_actividades")
+@EntityScan(basePackages = { "com.udea.registro_actividades.modelo" }, basePackageClasses = { Jsr310JpaConverters.class })
+@EnableJpaRepositories(basePackages = { "com.udea.registro_actividades.dao" })
+@EnableTransactionManagement
 public class Application { 	
 	
 	//Para correr con maven: mvn spring-boot:run
